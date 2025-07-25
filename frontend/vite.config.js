13e60bd76f3d3361805b8e1d4a90921ad3619345
jackie <jodonnell@arizona.edu>
2025-07-25 00:34:39 -0700

    get cloudflare worker to host my site for now

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,17 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+// Polyfill File constructor for build environments
+if (typeof global !== 'undefined' && typeof global.File === 'undefined') {
+  global.File = globalThis.File || class File extends Blob {
+    constructor(fileBits, fileName, options = {}) {
+      super(fileBits, options);
+      this.name = fileName;
+      this.lastModified = options.lastModified || Date.now();
+    }
+  };
+}
+
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	define: {
@@ -9,5 +20,8 @@ export default defineConfig({
 	},
 	optimizeDeps: {
 		include: ['@cloudflare/workers-types']
+	},
+	ssr: {
+		noExternal: ['@sveltejs/adapter-cloudflare']
 	}
 });
